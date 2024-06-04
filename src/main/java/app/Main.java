a201691e5110ef07624f731524bbcf2f8bf1ebbd
Jekbe <patrykrusakowicz@gmail.com>
2024-06-05 01:19:48 +0200

    chyba ok

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,16 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph(5);
 
-        // Dodaj krawędzie (przykładowe)
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(1, 3);
         graph.addEdge(1, 4);
 
-        // Uruchom algorytm Echo z węzłem inicjującym (np. 0)
         graph.runEcho(0);
 
-        // Wydrukuj drzewo rozpinające
         graph.printTree();
     }
 }
